5d88525128dc057d4c6de471c449e6c4caa39597
GeorgeAndzhich <djordjeandjic99@gmail.com>
2022-08-06 20:49:57 +0200

    ScaffoldDb try one

---
+++ PersonWindows/Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PersonWindows.Models
+{
+    public partial class Person
+    {
+        public short Id { get; set; }
+        public decimal Oid { get; set; }
+        public string NameAndSurname { get; set; } = null!;
+        public string Place { get; set; } = null!;
+        public string Address { get; set; } = null!;
+        public string Phone { get; set; } = null!;
+        public string Mail { get; set; } = null!;
+    }
+}
---
+++ PersonWindows/Models/PersonContext.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace PersonWindows.Models
+{
+    public partial class PersonContext : DbContext
+    {
+        public PersonContext()
+        {
+        }
+
+        public PersonContext(DbContextOptions<PersonContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Person> People { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=localhost;Database=Person;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Person>(entity =>
+            {
+                entity.Property(e => e.Id)
+                    .ValueGeneratedNever()
+                    .HasColumnName("ID");
+
+                entity.Property(e => e.Address)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Mail)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.NameAndSurname)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("Name and Surname");
+
+                entity.Property(e => e.Oid)
+                    .HasColumnType("numeric(11, 0)")
+                    .HasColumnName("OID");
+
+                entity.Property(e => e.Phone)
+                    .HasMaxLength(15)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Place)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
